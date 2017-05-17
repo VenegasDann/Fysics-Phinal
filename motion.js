b30b14690617bbff043f1e305d1bac169d67deea
Pom's Chicken <8dvenegas@stpatrick.org>
2017-05-17 11:27:34 -0500

    Continue
    Day 2

---
+++ motion.js
@@ -0,0 +1,15 @@
+var exports = module.exports = {};
+
+//use this prototype to create the other functions
+exports.distance = function(v,t){
+  return ;
+};
+exports.velocity = function(){
+  return ;
+};
+exports.acceleration = function(){
+  return ;
+};
+exports.time = function(){
+  return ;
+};
